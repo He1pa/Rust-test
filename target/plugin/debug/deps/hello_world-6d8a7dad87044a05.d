37d4f81e22355efeb5e209fd03dbec88ccff0a74
He1pa <18012015693@163.com>
2022-10-31 19:33:59 +0800

    zz

---
+++ target/plugin/debug/deps/hello_world-6d8a7dad87044a05.d
@@ -0,0 +1,5 @@
+/Users/zz/test/Rust-test/target/plugin/debug/deps/hello_world-6d8a7dad87044a05.rmeta: src/main.rs
+
+/Users/zz/test/Rust-test/target/plugin/debug/deps/hello_world-6d8a7dad87044a05.d: src/main.rs
+
+src/main.rs:

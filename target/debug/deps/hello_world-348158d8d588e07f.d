1a157c151355f22250fbac8b0dfb4587c911467f
He1pa <18012015693@163.com>
2022-10-31 19:28:41 +0800

    zz

---
+++ target/debug/deps/hello_world-348158d8d588e07f.d
@@ -0,0 +1,6 @@
+/Users/zz/test/Rust-test/target/debug/deps/hello_world-348158d8d588e07f.rmeta: src/main.rs src/tests.rs
+
+/Users/zz/test/Rust-test/target/debug/deps/hello_world-348158d8d588e07f.d: src/main.rs src/tests.rs
+
+src/main.rs:
+src/tests.rs:
---
+++ target/debug/deps/hello_world-7120d0bdeb7db231.d
@@ -0,0 +1,5 @@
+/Users/zz/test/Rust-test/target/debug/deps/hello_world-7120d0bdeb7db231.rmeta: src/main.rs
+
+/Users/zz/test/Rust-test/target/debug/deps/hello_world-7120d0bdeb7db231.d: src/main.rs
+
+src/main.rs:
